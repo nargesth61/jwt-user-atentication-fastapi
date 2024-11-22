f1000480245f8fc7da8c3ad94d4475819663b91d
nargesth61 <107623472+nargesth61@users.noreply.github.com>
2024-11-22 04:10:12 -0800

    Update database.py

--- main_project/app/database.py
+++ main_project/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.settings import DATABASE_URL
 
-# اتصال به پایگاه داده
+# setup database
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
